ce6af5eab9b79649e11f533b9d3f79be9a6c5f81
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-03-29 11:34:02 +0100

    Building granulator. Untested

--- Source/Granulator.cpp
+++ Source/Granulator.cpp
@@ -1,8 +1,60 @@
+#define _USE_MATH_DEFINES
+ 
 #include "Granulator.h"
+#include <cmath>
+Granulator::Granulator(const int maxBufSize) 
+{
+    // Create buffer to store a set amount of audio - this will be used as the
+    // source for grains of output
+    grainBuf_ = AudioSampleBuffer(1, maxBufSize);
+    // Create a write pointer to use for writing audio to the buffer
+    grainBufWritePointer_ = grainBuf_.getWritePointer(0);
+}
+Granulator::updateParameters(const unsigned int grainSize, const unsigned int hopSize) : hopSize_(hopSize), grainSize_(grainSize) 
+{
+    // For the number of overlapping grains at any time
+    for(int i=0; i<ceil(float(grainSize_)/float(hopSize_)); i++) {
+        // Create a read pointer
+        grainBufReadPointers_.push_back(grainBuf_.getReadPointer(0));
+        // A position of -1 indicates that the pointer is inactive
+        grainBufReadPointersPosition_.push_back(-1);
+    }
+    // For the number of samples in a grain...
+    for(int n=0; n<grainSize_; n++) {
+        //TODO: Check accuracy of this is correct
+        // Fill a vector with values for a hanning window at the current grain
+        // size
+        windowBuf.push_back();
+    }
+}
+
 void Granulator::applyShuffle (const float* const in, float* const out, const int numSamples) noexcept {
+    // For each sample in the current input block...
     for(int i = 0; i < numSamples; ++i) {
-        // Perform filtering using doubles for greater precision
-        const double inSamp = in[i];
-        out[i] = inSamp;
+        // Write input sample to current point in buffer
+        *grainBufWritePointer_ = in[i];
+
+        // For each read pointer for the current channel...
+        for(int j=0; j<grainBufReadPointers_.size(); j++) {
+            // If the read pointer isn't currently active...
+            if(grainBufReadPointersPosition_[j] < 0) {
+                // Generate new index at random within the input sample buffer
+                int randNum = randomFrom<int>(0, int(grainBuf.size()));
+                // Set read position of the current pointer to that index
+                grainBufReadPointers_[j] = grainBuf_.getReadPointer(0, randNum);
+            }
+
+            // Generate value for hann window at the current index
+            float winVal = 0.5*(1.0-cos((2*M_PI*grainBufReadPointersPosition_[j])/(grainSize_-1)
+            // read current sample into output and multiply by the window
+            out[i] = inSamp;
+            // function at the current index
+            // Increment read pointer and check that it hasn't exceeded the size of
+            // the grain
+            // If it has then reset it's position to inactive
+        }
+        // Increment the write pointer
+        (inputDelayBufWritePtr+inputDelaySize)%inputDelaySize;
+
     }
 }
--- Source/Granulator.h
+++ Source/Granulator.h
@@ -2,14 +2,41 @@
 #define __GRANULATOR_H_4693CB6E__
 
 #include "../JuceLibraryCode/JuceHeader.h"
+#include <vector>
 
 class Granulator
 {
     public:
-        Granulator() {};
+        Granulator(const int numChannels, const int maxBufSize);
         ~Granulator() {};
+        void updateParameters();
         void applyShuffle(const float* const in, float* const out, const int numSamples) noexcept;
     private:
+        AudioSampleBuffer grainBuf_;
+        vector<*float> grainBufReadPointers_;
+        vector<int> grainBufReadPointersPosition_;
+        *float grainBufWritePointer_;
+
+        unsigned int grainSize_;
+        unsigned int hopSize_;
+
+        vector<float> winowBuf_;
+
+        // Random number generator objects
+        static std::random_device rdev;
+        static std::default_random_engine re(rdev());
+        // Code adapted from: http://stackoverflow.com/questions/5743678/generate-random-number-between-0-and-10
+        template <typename T>
+        T randomFrom(const T min, const T max)
+        {
+            typedef typename std::conditional<
+                std::is_floating_point<T>::value,
+                std::uniform_real_distribution<T>,
+                std::uniform_int_distribution<T>>::type dist_type;
+            dist_type uni(min, max);
+            return static_cast<T>(uni(re));
+        }
+
         JUCE_LEAK_DETECTOR (Granulator);
 };
 #endif  // __GRANULATOR_H_4693CB6E__
